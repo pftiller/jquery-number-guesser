2b90dd712d3eaf2e18b6fd182faf57a3d4f2de0d
pftiller <pftiller@gmail.com>
2017-12-15 11:37:29 -0600

    Write out psuedo code

---
+++ client.js
@@ -0,0 +1,15 @@
+//Define our Number
+
+//Get user value on button click
+
+//Compare user value to our Number
+
+//If correct, then update text and change background color to green
+
+//If not correct, check if greater or less than our number
+
+//If user value is greater, print 'Guess Lower' and change background color to yelllow
+
+//If user value is less, print 'Guess Higher' and change background color to orange
+
+//*HARD MODE* Add a new button to reset game

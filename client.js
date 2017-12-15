105a7a336a4a892a6ee37756f3878cddbcf97692
pftiller <pftiller@gmail.com>
2017-12-15 12:00:38 -0600

    User number checks

--- client.js
+++ client.js
@@ -1,7 +1,29 @@
-//Define our Number
+$(document).ready(numberGuesserGame);
+
+function numberGuesserGame() {
+    //event listener
+    $('#guess_button').on('click', getUserNumber);
+
+  //Define our Number
+  var theNumber = 7;
 
 //Get user value on button click
+function getUserNumber() {
+  var userNumber = $('#guess_input').val();
 
+  console.log(userNumber);
+//Check to see if user value is between 1 and 10
+if(userNumber % 1 == 0 && userNumber > 0 && userNumber < 11) {
+    return userNumber;
+    }
+    //If not between 1 and 10, alert user (no decimals)
+    else {
+      alert('Number must be a whole number and between 1 and 10');
+    }
+
+
+  $('#guess_input').val('').focus();//clear input
+}
 //Compare user value to our Number
 
 //If correct, then update text and change background color to green
@@ -11,5 +33,5 @@
 //If user value is greater, print 'Guess Lower' and change background color to yelllow
 
 //If user value is less, print 'Guess Higher' and change background color to orange
-
+}
 //*HARD MODE* Add a new button to reset game
